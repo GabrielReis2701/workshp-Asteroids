c0854f39e7748142ea483aa490247599407f436c
Gabriel Reis <gabrielreis2701@gmail.com>
2021-09-17 17:33:10 -0400

    Adicionado comportamento aos objetos
    foi adicionado o comportamento do jogador dos asteroides e dos projeteis da nave

---
+++ Assets/Scripts/ComportamentoJogador.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamentoJogador : MonoBehaviour
+{
+    public Rigidbody2D meuRigidbody;
+    public float aceleracao;
+    public float velocidadeAngular= 180.0f;
+    public float velocidadeMaxima = 10.0f;
+    public Rigidbody2D prefabTiroNave;
+    public float velocidadeTiro = 10.0f;
+
+    void Start()
+    {
+        
+    }
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+           Rigidbody2D TiroNave = Instantiate(prefabTiroNave, meuRigidbody.position, Quaternion.identity);
+           TiroNave.velocity = transform.up * velocidadeTiro;
+        }
+    }
+    void FixedUpdate()
+    {
+       if(Input.GetKey(KeyCode.UpArrow)){
+           Vector3 direcao =transform.up * aceleracao;
+           meuRigidbody.AddForce(direcao, ForceMode2D.Force);
+       } 
+       if(Input.GetKey(KeyCode.LeftArrow)){
+           meuRigidbody.rotation += velocidadeAngular * Time.deltaTime;
+       }
+       if(Input.GetKey(KeyCode.RightArrow)){
+           meuRigidbody.rotation -= velocidadeAngular * Time.deltaTime;
+       }
+       if(Input.GetKey(KeyCode.W)){
+           Vector3 direcao =transform.up * aceleracao;
+           meuRigidbody.AddForce(direcao, ForceMode2D.Force);
+       } 
+       if(Input.GetKey(KeyCode.A)){
+           meuRigidbody.rotation += velocidadeAngular * Time.deltaTime;
+       }
+       if(Input.GetKey(KeyCode.D)){
+           meuRigidbody.rotation -= velocidadeAngular * Time.deltaTime;
+       }
+       if(meuRigidbody.velocity.magnitude > velocidadeMaxima){
+           meuRigidbody.velocity = Vector2.ClampMagnitude(meuRigidbody.velocity,velocidadeMaxima);
+
+       }
+    }
+    void OnTriggerEnter2D(Collider2D outro){
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/OndaDeAsteroides.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OndaDeAsteroides : MonoBehaviour
+{
+    public ComportamentoAsteroide prefabAsteroide;
+    public int quantAsteroides= 3;
+    void Start()
+    {
+        for(int i=0;i<quantAsteroides;i++){
+            float x = Random.Range(-9.0f, 9.0f);
+            float y = Random.Range(-3.0f, 3.0f);
+            Vector3 posicao = new Vector3(x, y, 0.0f);
+            Instantiate(prefabAsteroide, posicao, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
